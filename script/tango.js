b60e539bdd60271d4417557e5345de0360b7c591
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-07-21 15:08:29 -0700

    f

--- script/tango.js
+++ script/tango.js
@@ -1,22 +1,21 @@
 function createItemElem(item) {
-
     return `<div class="item">
         <img src="/images/tango/${item.image}" />
         <div class="item-name">${item.name}</div>
         <div class="item-price">${item.price}</div>
-        <button class="add-to-cart" onclick="addToCart("${item.name}")">Add to Cart</button>
+        <button class="add-to-cart" onclick='addToCart("${item.name}")'>Add to Cart</button>
     </div>`;
 }
 
 function appendItemElem(html) {
-    $("#store").html($("store").html() + html);
+    $("#store").append(html);
 }
 
 const appendItem = (item) => appendItemElem(createItemElem(item));
 
 var items = [];
 
-$(document).ready(e => {
+$(document).ready(() => {
     fetch("/tango_store.json")
     .then(response => response.json())
     .then(data => {

81fd77049e3514b1e5cd21b3b1be03fb9b21ec13
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-07-21 22:50:09 -0700

    f312

--- script/cursor.js
+++ script/cursor.js
@@ -16,8 +16,8 @@ styleElem.innerHTML = `
   z-index: 9999;
 }`
 
-document.getElementsByTagName("head").appendChild(styleElem);
-document.getElementsByTagName("body").appendChild(cursor);
+document.getElementsByTagName("head")[0].appendChild(styleElem);
+document.getElementsByTagName("body")[0].appendChild(cursor);
 
 document.addEventListener('mousemove', e => {
     cursor.style.left = e.pageX + 'px';

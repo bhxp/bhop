876c306ad23bb76fc7a3dcd5a61041717b1f17db
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-07-21 21:06:14 -0700

    ffr3qf

--- script/tango.js
+++ script/tango.js
@@ -2,8 +2,8 @@ function createItemElem(item) {
     return `<div class="item">
         <img src="/images/tango/${item.image}" class="item-img" />
         <div class="item-name">${item.name}</div>
-        <div class="item-price">${item.price}</div>
-        <button class="add-to-cart" onclick='addToCart("${"$" + item.name}")'>Add to Cart</button>
+        <div class="item-price">$${item.price}</div>
+        <button class="add-to-cart" onclick='addToCart("${item.name}")'>Add to Cart</button>
     </div>`;
 }
 

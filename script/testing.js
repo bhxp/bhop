81f754033dd05746dc6036ec2389f2544724eddf
bhxp <146797792+bhxp@users.noreply.github.com>
2024-07-21 21:56:19 -0700

    Create testing.js

---
+++ script/testing.js
@@ -0,0 +1,8 @@
+// Alert Button Functionality
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Alert Button Clicked!');
+});
+
+// Change Color Button Functionality
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    document

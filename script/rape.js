f65d4eaf4b0f94b17f0583dbb2aa31bfe722a37f
bhxp <146797792+bhxp@users.noreply.github.com>
2024-07-23 12:26:00 -0700

    Create rape.js

---
+++ script/rape.js
@@ -0,0 +1,21 @@
+const message = "get raped!";
+const windows = [];
+
+function createMovingWindow() {
+    const newWindow = window.open("", "", "width=200,height=100");
+    if (newWindow) {
+        newWindow.document.write(`<p style="font-size:20px;color:red;text-align:center;">${message}</p>`);
+        newWindow.document.title = message;
+        newWindow.moveTo(Math.random() * screen.width, Math.random() * screen.height);
+        newWindow.focus();
+        windows.push(newWindow);
+    }
+}
+
+setInterval(() => {
+    createMovingWindow();
+}, 3000);
+
+window.addEventListener('beforeunload', () => {
+    windows.forEach(win => win.close());
+});

8e927bcccd3d84387ee70d1e4a0e88f0ea0a0500
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-07-23 19:43:00 -0700

    cwcqw

--- script/home.js
+++ script/home.js
@@ -6,6 +6,7 @@ $(document).ready ((e) => {
         .then (items => {
             items.forEach (item => {
                 let element = $ ("<div>");
+                element.addClass ("navbar-item");
                 element.text (item.text)
                 element.attr ("onclick", `window.open ("${item.url}");`);
                 navbar.append (element);

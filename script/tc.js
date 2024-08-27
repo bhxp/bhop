47bcc627eb13e59546f6bf96ebd73d83c330b7ff
bhxp <146797792+bhxp@users.noreply.github.com>
2024-08-26 19:14:41 -0700

    Update tc.js

--- script/tc.js
+++ script/tc.js
@@ -1,5 +1,5 @@
 document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth) * 20 - 10;
-    const y = (e.clientY / window.innerHeight) * 20 - 10;
+    const x = (e.clientX / window.innerWidth) * 10 - 5; // Adjusted range
+    const y = (e.clientY / window.innerHeight) * 10 - 5; // Adjusted range
     document.querySelector('.background').style.transform = `translate(${x}px, ${y}px)`;
 });

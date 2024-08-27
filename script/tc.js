eb746d68f1d1bc5852ac1ba1ea36cb92bf806e22
bhxp <146797792+bhxp@users.noreply.github.com>
2024-08-26 19:18:51 -0700

    Update tc.js

--- script/tc.js
+++ script/tc.js
@@ -1,5 +1,5 @@
 document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth) * 10 - 5; // Adjusted range
-    const y = (e.clientY / window.innerHeight) * 10 - 5; // Adjusted range
+    const x = (e.clientX / window.innerWidth) * 20 - 10; // Adjust range for parallax
+    const y = (e.clientY / window.innerHeight) * 20 - 10; // Adjust range for parallax
     document.querySelector('.background').style.transform = `translate(${x}px, ${y}px)`;
 });

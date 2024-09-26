f1cdf934e31c874894aaf8a1188e00ace7aa6de5
ChezLinkZ <143735617+ChezLinkZ@users.noreply.github.com>
2024-09-26 11:53:28 -0700

    chatgpt please save me

--- script/home.js
+++ script/home.js
@@ -1,57 +1,70 @@
 const navbar = $("#navbar");
 var navbarItems = null;
 var cancelNavbarHide = true;
-
+var hideTimeout = null; // track the hide timeout
 
 $(document).on("mousedown", e => {
-    setTimeout(() => {
+    if (hideTimeout) {
+        clearTimeout(hideTimeout);  // Clear any existing hide timeout
+    }
+
+    hideTimeout = setTimeout(() => {
         console.log("dropdown hidden");
         if (!cancelNavbarHide) {
             $("#navbar div.dropdown").addClass("hidden");
-        } else {
-            cancelNavbarHide = false;
         }
     }, 50);
 });
 
 function openDropdown(index) {
     cancelNavbarHide = true;
-    console.log(navbar.find("div.dropdown"))
-    $("#navbar div.dropdown")[index].classList.remove("hidden")
-    console.log("dropdown shown")
+
+    // Clear the hide timeout if the dropdown is opened
+    if (hideTimeout) {
+        clearTimeout(hideTimeout);
+    }
+
+    console.log(navbar.find("div.dropdown"));
+    $("#navbar div.dropdown")[index].classList.remove("hidden");
+    console.log("dropdown shown");
+
+    // Reset cancelNavbarHide after a short delay to avoid conflict with hide logic
+    setTimeout(() => {
+        cancelNavbarHide = false;
+    }, 100);  // Slightly longer than the hide delay to avoid conflicts
 }
 
-$(document).ready ((e) => {
-    fetch ("/config/home_navbar.json")
-        .then (response => response.json())
-        .then (items => {
+$(document).ready((e) => {
+    fetch("/config/home_navbar.json")
+        .then(response => response.json())
+        .then(items => {
             navbarItems = items;
-            items.forEach (item => {
+            items.forEach(item => {
                 if (item.multiple) {
-                    console.log("multiple items")
-                    let element = $ ("<div class='navbar-item'></div>");
-                    element.text (item.text)
-                    element.attr ("onclick", `openDropdown(${items.indexOf(item)})`);
-                    let dropdown = $("<div class='dropdown'></div>")
+                    console.log("multiple items");
+                    let element = $("<div class='navbar-item'></div>");
+                    element.text(item.text);
+                    element.attr("onclick", `openDropdown(${items.indexOf(item)})`);
+                    let dropdown = $("<div class='dropdown hidden'></div>");
                     item.pages.forEach(page => {
-                        let elem = $ ("<div>");
-                    elem.addClass ("navbar-item");
-                    elem.text (page.text)
-                    elem.attr ("onclick", `window.open ("${page.url}");`);
-                    dropdown.append (elem);
-                    })
+                        let elem = $("<div>");
+                        elem.addClass("navbar-item");
+                        elem.text(page.text);
+                        elem.attr("onclick", `window.open("${page.url}");`);
+                        dropdown.append(elem);
+                    });
                     element.append(dropdown);
-                    navbar.append (element);
+                    navbar.append(element);
                 } else {
-                    let element = $ ("<div>");
-                    element.addClass ("navbar-item");
-                    element.text (item.text)
-                    element.attr ("onclick", `window.open ("${item.url}");`);
-                    navbar.append (element);
+                    let element = $("<div>");
+                    element.addClass("navbar-item");
+                    element.text(item.text);
+                    element.attr("onclick", `window.open("${item.url}");`);
+                    navbar.append(element);
                 }
             });
         })
-        .catch (error => {
-            console.error (error);
+        .catch(error => {
+            console.error(error);
         });
 });

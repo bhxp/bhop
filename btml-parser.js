11144e1ab64e8b09e800a297e1ec1ae7e19c6e24
bhxp <146797792+bhxp@users.noreply.github.com>
2024-11-10 15:51:18 -0800

    Update btml-parser.js

--- btml-parser.js
+++ btml-parser.js
@@ -4,7 +4,9 @@
 // a simplified html for idiots!
 
 function parseBTML(btmlContent) {
-  return btmlContent
+  console.log("Parsing BTML content...");
+
+  const parsedContent = btmlContent
     .replace(/<img=([^>]+)>/g, '<img src="$1">')
     .replace(/<text>([^<]+)<\/text>/g, '<span>$1</span>')
     .replace(/<titletext>([^<]+)<\/titletext>/g, '<h1>$1</h1>')
@@ -23,10 +25,14 @@ function parseBTML(btmlContent) {
 
     .replace(/<linebreak>/g, '<br>')
     .replace(/<embed (\d+)px, (\d+)px>([^<]+)<\/embed>/g, '<iframe src="$3" width="$1" height="$2"></iframe>');
+
+  console.log("Parsed Content:", parsedContent);
+  return parsedContent;
 }
 
 document.querySelectorAll('script[type="text/btml"]').forEach(script => {
   const btmlContent = script.innerHTML;
+  console.log("Found BTML script:", btmlContent);  // Log content found in <script> tag
   const parsedHTML = parseBTML(btmlContent);
   document.getElementById("output").innerHTML += parsedHTML;
 });
